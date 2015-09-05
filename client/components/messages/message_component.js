356ee31b42e91c6fae06b30d924048a885444ae2
Gaurav Agarwal <gauravagarwalr@gmail.com>
2015-09-05 23:26:28 +0530

    Styling the messages.

--- client/components/messages/message_component.js
+++ client/components/messages/message_component.js
@@ -1,8 +1,9 @@
 import React from "react";
 import lodash from "lodash";
-import classNames from "classnames";
 
 import shouldComponentUpdate from "../common/shouldupdate";
+
+import PhotoComponent from "../shared/photo_component";
 
 import User from "../../stores/users_store";
 import Message from "../../stores/messages_store";
@@ -25,16 +26,54 @@ class MessageComponent extends React.Component {
     }
   }
 
-  render() {
+  renderMessage() {
+    var message = this.props.message;
+
+    return (
+      <div className="col-md-10 message-content">
+        <div className="message-body">
+          {Message.body(message)}
+        </div>
+        <div className="message-details">
+          {Message.getTime(message).fromNow()}
+        </div>
+      </div>
+    );
+  }
+
+  renderFromDetails() {
     var message = this.props.message;
     var messageFrom = Message.fromUser(message);
 
     return (
-      <div className={classNames("message", {"sent-by-current-user": User.isCurrentUser(messageFrom)})}>
-        <div className="message-user">{User.name(messageFrom)}</div>
-        <div className="message-body">{Message.body(message)}</div>
+      <div className="col-md-2 user-details">
+        <PhotoComponent url={User.photoUrl(messageFrom)}/>
+        <div className="username">
+          {User.username(messageFrom)}
+        </div>
       </div>
     );
+  }
+
+  render() {
+    var message = this.props.message;
+    var messageFrom = Message.fromUser(message);
+
+    if(User.isCurrentUser(messageFrom)) {
+      return (
+        <div className="row message message-alpha">
+          {this.renderMessage()}
+          {this.renderFromDetails()}
+        </div>
+      );
+    } else {
+      return (
+        <div className="row message message-beta">
+          {this.renderFromDetails()}
+          {this.renderMessage()}
+        </div>
+      );
+    }
   }
 }
 
